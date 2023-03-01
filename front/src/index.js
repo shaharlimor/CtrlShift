1d40ba60bf20d2ff964b30f708a92e7af01132ca
Sagi Ben Mordechai <79403955+sagi21333@users.noreply.github.com>
2023-03-01 15:38:49 +0200

    adding metrial theme

--- front/src/index.js
+++ front/src/index.js
@@ -1,17 +1,33 @@
+import { createRoot } from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// third party
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+// project imports
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BASE_PATH } from './config';
+import { ConfigProvider } from './contexts/ConfigContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// style + assets
+import './assets/scss/style.scss';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // <Provider store={store}>
+    // <PersistGate loading={null} persistor={persister}>
+    // <ConfigProvider>
+    //<BrowserRouter basename={BASE_PATH}>
+    //<App />
+    // </BrowserRouter>
+    // </ConfigProvider>
+    // </PersistGate>
+    // </Provider>
+);
