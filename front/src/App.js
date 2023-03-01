79c59d93ef07ee1f59c443e4069bed16bf5861d4
Sagi Ben Mordechai <79403955+sagi21333@users.noreply.github.com>
2023-03-01 15:47:31 +0200

    adding metrial theme

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
-
+import BackgroundPattern1 from './components/BackgroundPattern1';
 import ThemeCustomization from './themes';
 
 const App = () => {
-    return <ThemeCustomization></ThemeCustomization>;
+    return (
+        <ThemeCustomization>
+            <BackgroundPattern1 />
+        </ThemeCustomization>
+    );
 };
 
 export default App;
