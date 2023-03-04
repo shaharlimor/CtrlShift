e801748a3cc556b3a9580e36bf468c5bb4f910a8
Sagi Ben Mordechai <79403955+sagi21333@users.noreply.github.com>
2023-03-04 20:02:54 +0200

    Update AuthLogin.js

--- front/src/components/auth/AuthLogin.js
+++ front/src/components/auth/AuthLogin.js
@@ -96,7 +96,7 @@ const FirebaseLogin = ({ loginProp, ...others }) => {
                 {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
                     <form noValidate onSubmit={handleSubmit} {...others}>
                         <FormControl fullWidth error={Boolean(touched.email && errors.email)} sx={{ ...theme.typography.customInput }}>
-                            <InputLabel htmlFor="outlined-adornment-email-login">Email Address / Username</InputLabel>
+                            <InputLabel htmlFor="outlined-adornment-email-login">Email Address</InputLabel>
                             <OutlinedInput
                                 id="outlined-adornment-email-login"
                                 type="email"
