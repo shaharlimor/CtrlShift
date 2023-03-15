95eb171524ca2055e5b0010822a6635604712c74
Hadas <hadas716@gmail.com>
2023-03-15 11:11:56 +0200

    fixing toutes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,36 +3,12 @@ const authController = require('../controllers/auth');
 const middleware = require('../common/auth_middleware');
 var router = express.Router();
 
-router.get('/login', (req, res) => {
-    authController.login().then(users => {
-        // res.send(users);
-    }).catch(error => {
-        res.status(500).send({error: error.message});
-    });
-});
-
-router.get('/register', (req, res) => {
-    authController.register()
-    .catch(error => {
-        res.status(500).send({error: error.message});
-    });
-});
-
-router.get('/refreshToken', (req, res) => {
-    authController.refreshToken()
-    .catch(error => {
-        res.status(500).send({error: error.message});
-    });
-});
+router.post('/login', authController.login);
+router.get('/register', authController.register);
+router.get('/refreshToken', authController.refreshToken);
 
 // With middleware
-router.get('/logout', middleware, (req, res) => {
-    authController.logout()
-    .catch(error => {
-        res.status(500).send({error: error.message});
-    });
-});
-
+router.get('/logout', middleware, authController.logout);
 
 
 module.exports = router;
