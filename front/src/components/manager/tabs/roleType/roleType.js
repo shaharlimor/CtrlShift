3cfa844896071b07f784145eb2dd8adfdc369aa5
ayabryl <ayabryl@gmail.com>
2023-03-21 18:00:31 +0200

    add role type form

--- front/src/components/manager/tabs/roleType/roleType.js
+++ front/src/components/manager/tabs/roleType/roleType.js
@@ -1,23 +1,36 @@
 import RoleTypesList from 'components/manager/tabs/roleType/roleTypeList';
 import MainCard from 'components/cards/MainCard';
-
+import React, { useEffect } from 'react';
+// eslint-disable-next-line
 import AddCircleOutlineTwoToneIcon from '@mui/icons-material/AddCircleOutlineTwoTone';
 
 import { Button } from '@mui/material';
+import RoleTypeForm from './roleTypeForm';
 
 /* eslint-disable */
-const RoleTypes = () => (
-    <MainCard title="RoleTypes">
-        <RoleTypesList />
-        <Button
+const RoleTypes = () => {
+    // eslint-disable-next-line
+    const [showForm, setShowForm] = React.useState(false);
+
+    const changeShowForm = () => {
+        setShowForm(!showForm);
+    } ;
+
+
+    return (<MainCard title="RoleTypes">
+         {showForm &&(<RoleTypeForm changeShowForm={changeShowForm}/>)}
+         {!showForm &&(<RoleTypesList />)}
+         {!showForm &&(<Button
                 variant="contained"
                 sx={{ width: '15%' }}
+                onClick={changeShowForm}
                 size="large"
                 startIcon={(<AddCircleOutlineTwoToneIcon />)}>
                Add Role Type
-            </Button>
-    </MainCard>
-);
+            </Button>)}
+
+    </MainCard>)
+};
 /* eslint-disable */
 
 // eslint-disable-next-line
