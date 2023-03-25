57681efcf55e6fa8da57defc35e882bb18ef8182
Sagi Ben Mordechai <79403955+sagi21333@users.noreply.github.com>
2023-03-25 10:12:29 +0300

    fix layout components in permenent shift

---
+++ front/src/components/manager/AddPermenentShift.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import {
+    Box,
+    Collapse,
+    IconButton,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+    Grid
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
+import AddIcon from '@mui/icons-material/Add';
+
+// project imports
+import MainCard from '../cards/MainCard';
+import SubCard from '../cards/SubCard';
+
+// assets
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+export default function AddPermenentShift() {
+    const theme = useTheme();
+
+    return <p> add</p>;
+}
--- front/src/components/manager/PermenentShifts.js
+++ front/src/components/manager/PermenentShifts.js
@@ -24,148 +24,42 @@ import AddIcon from '@mui/icons-material/Add';
 // project imports
 import MainCard from '../cards/MainCard';
 import SubCard from '../cards/SubCard';
-
-// assets
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-
-// table data
-function createData(name, startTime, endTime, days, roles) {
-    return {
-        name,
-        startTime,
-        endTime,
-        days,
-        roles: roles.map((role) => ({
-            roleType: role[0],
-            amount: role[1]
-        }))
-    };
-}
-
-function Row({ row }) {
-    const handleEditClick = () => {
-        console.log('Edit row:', row);
-        // TODO: Implement edit
-    };
-
-    const handleDeleteClick = () => {
-        console.log('Delete row:', row);
-        // TODO: Implement delete
-    };
-
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-
-    return (
-        <>
-            <TableRow hover sx={{ '& > *': { borderBottom: 'unset' } }}>
-                <TableCell sx={{ pl: 3 }}>
-                    <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </TableCell>
-                <TableCell component="th" scope="row">
-                    {row.name}
-                </TableCell>
-                <TableCell>{row.startTime}</TableCell>
-                <TableCell>{row.endTime}</TableCell>
-                <TableCell>{row.days}</TableCell>
-                <TableCell sx={{ pr: 3 }}>
-                    <IconButton color="primary" size="large" onClick={handleDeleteClick}>
-                        <DeleteIcon sx={{ fontSize: '1.3rem' }} />
-                    </IconButton>
-                    <IconButton color="secondary" size="large" onClick={handleEditClick}>
-                        <EditTwoToneIcon sx={{ fontSize: '1.3rem' }} />
-                    </IconButton>
-                </TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        {open && (
-                            <Box sx={{ margin: 1 }}>
-                                <TableContainer>
-                                    <SubCard
-                                        sx={{ bgcolor: theme.palette.mode === 'dark' ? 'dark.800' : 'grey.50', mb: 2 }}
-                                        title="roles"
-                                        content={false}
-                                    >
-                                        <Stack direction="row" spacing={2} alignItems="center" sx={{ mr: 2, ml: 2 }}>
-                                            {row.roles?.map((rolesRow) => (
-                                                <Typography>
-                                                    {rolesRow.roleType} - {rolesRow.amount}
-                                                </Typography>
-                                            ))}
-                                        </Stack>
-                                    </SubCard>
-                                </TableContainer>
-                            </Box>
-                        )}
-                    </Collapse>
-                </TableCell>
-            </TableRow>
-        </>
-    );
-}
-
-Row.propTypes = {
-    row: PropTypes.object
-};
-
-const rows = [
-    createData('Day - evening', '7pm', '8am', 'SUNDAY', [
-        ['chef', 1],
-        ['waiter', 2]
-    ])
-];
+import PermenentShiftTableMode from './PermenentShiftTableMode';
+import AddPermenentShift from './AddPermenentShift';
 
 export default function PermanentShift() {
     const theme = useTheme();
 
-    const newRow = [];
-    rows.forEach((element) => {
-        newRow.push({
-            ...element,
-            history: null
-        });
-    });
+    const [addOpen, setAddOpen] = React.useState(false);
 
     return (
-        <MainCard content={false} title="Permanent Shift">
-            {/* table */}
-            <Grid container direction="column" justifyContent="center" alignItems="flex-end">
-                <TableContainer>
-                    <Table aria-label="collapsible table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell sx={{ pl: 3 }} />
-                                <TableCell>Name</TableCell>
-                                <TableCell>Start Time</TableCell>
-                                <TableCell>End Time</TableCell>
-                                <TableCell>Days</TableCell>
-                                <TableCell>Action</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <Row key={row.name} row={row} />
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-
-                <Grid item xs={3} sx={{ mt: 2 }}>
-                    <IconButton
-                        variant="contained"
-                        sx={{ fontSize: '35px', bgcolor: theme.palette.secondary.main, color: theme.palette.secondary.contrastText }}
-                        // onClick={}
-                        size="large"
-                    >
-                        <AddIcon />
-                    </IconButton>
-                </Grid>
-            </Grid>
+        <MainCard content={false} title={!addOpen ? 'Permanent Shift' : 'Add Permenent Shift'}>
+            {!addOpen ? (
+                <>
+                    {' '}
+                    <PermenentShiftTableMode />
+                    <Grid container direction="column" justifyContent="center" alignItems="flex-end">
+                        <Grid item xs={3} sx={{ mt: 2 }}>
+                            <IconButton
+                                variant="contained"
+                                sx={{
+                                    fontSize: '35px',
+                                    bgcolor: theme.palette.secondary.main,
+                                    color: theme.palette.secondary.contrastText
+                                }}
+                                onClick={() => {
+                                    setAddOpen(true);
+                                }}
+                                size="large"
+                            >
+                                <AddIcon />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </>
+            ) : (
+                <AddPermenentShift />
+            )}
         </MainCard>
     );
 }
