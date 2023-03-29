35297d57c157bff230925a73a8972bd65f3912a2
Hadas <hadas716@gmail.com>
2023-03-29 23:12:23 +0300

    estlint remove

--- front/src/utils/axios.js
+++ front/src/utils/axios.js
@@ -7,21 +7,22 @@ const defaultOptions = {
 };
 const axiosServices = axios.create(defaultOptions);
 
-/* eslint-disable */
 axiosServices.interceptors.response.use(
     (response) => response,
     (error) => {
         const originalRequest = error.config;
 
+        /* eslint-disable-next-line */
         if (error.response.status === 401 && !originalRequest._retry) {
+            /* eslint-disable-next-line */
             originalRequest._retry = true;
 
-            // return refreshAccessToken().then(() => {
-            //     // Update the new tokens
-            //     originalRequest.headers['accessToken'] = axios.defaults.headers.common.accessToken;
-            //     originalRequest.headers['refreshToken'] = axios.defaults.headers.common.refreshToken;
-            //     return axiosServices.request(originalRequest);
-            // });
+            return refreshAccessToken().then(() => {
+                // Update the new tokens
+                originalRequest.headers.accessToken = axios.defaults.headers.common.accessToken;
+                originalRequest.headers.refreshToken = axios.defaults.headers.common.refreshToken;
+                return axiosServices.request(originalRequest);
+            });
         }
 
         return Promise.reject((error.response && error.response.data) || 'Wrong Services');
