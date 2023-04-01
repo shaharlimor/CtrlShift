5cd7009b3c5667e804feab667939ac7aa84ac780
Hadas <hadas716@gmail.com>
2023-04-01 12:12:44 +0300

    fixing

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ const userController = require("../controllers/users");
 const middleware = require('../common/auth_middleware');
 var router = express.Router();
 
-router.post('/delete/:id', middleware, userController.deleteEmployee);
+router.post('/delete/:id', middleware, userController.deleteUser);
 router.post('/create', middleware, userController.createUser);
 router.post('/update/:id', middleware, userController.updateUser);
 
