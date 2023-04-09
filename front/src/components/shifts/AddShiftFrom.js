f896a2220b2aa97a93f6d081bd8b0cdd4bb911a0
shahar <shaharl44@gmail.com>
2023-04-09 11:12:55 -0700

    change form layout

--- front/src/components/shifts/AddShiftFrom.js
+++ front/src/components/shifts/AddShiftFrom.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 
-import { Button, DialogActions, DialogContent, DialogTitle, Divider, Grid, TextField, InputAdornment } from '@mui/material';
+import { Button, DialogActions, DialogContent, DialogTitle, Divider, Grid, TextField, InputAdornment, IconButton } from '@mui/material';
 import { LocalizationProvider, MobileDateTimePicker } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 import * as Yup from 'yup';
-import { useFormik, Form, FormikProvider } from 'formik';
+import { useFormik, Form, FormikProvider, Field, FieldArray } from 'formik';
 import { Fragment } from 'react';
 // project imports
 import { gridSpacing } from 'store/constant';
@@ -13,6 +13,8 @@ import { addMonthlyShift } from 'utils/api';
 import useAuth from 'hooks/useAuth';
 import { useTheme } from '@mui/material/styles';
 import DateRangeIcon from '@mui/icons-material/DateRange';
+
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 
 const AddShiftFrom = ({ onCancel }) => {
     const { user } = useAuth();
@@ -24,7 +26,14 @@ const AddShiftFrom = ({ onCancel }) => {
             .when('start', (start, schema) => start && schema.min(start, 'End date must be later than start date'))
             .required('End date is required'),
         start: Yup.date().required('Start date is required'),
-        roles: Yup.array().min(1, 'roles is required')
+        roles: Yup.array()
+            .of(
+                Yup.object().shape({
+                    type: Yup.string().required('Role type is required'), // these constraints take precedence
+                    amount: Yup.number().required('Amount of employess is required') // these constraints take precedence
+                })
+            )
+            .min(1, 'Minimum of 1 role is required')
     });
 
     const formik = useFormik({
@@ -38,7 +47,6 @@ const AddShiftFrom = ({ onCancel }) => {
         onSubmit: async (values, { resetForm, setSubmitting }) => {
             // TODO: first check if permanent shifts for this month and year already generated.
             try {
-                /* eslint-disable */
                 console.log(values);
                 // const data = {
                 //     organization: user.organization,
@@ -59,7 +67,7 @@ const AddShiftFrom = ({ onCancel }) => {
     });
 
     // const { errors, touched, handleSubmit, getFieldProps } = formik;
-    const { values, errors, touched, handleSubmit, isSubmitting, getFieldProps, setFieldValue } = formik;
+    const { values, errors, touched, handleSubmit, isSubmitting, getFieldProps, setFieldValue, handleArrayPush } = formik;
 
     return (
         <FormikProvider value={formik}>
@@ -68,7 +76,7 @@ const AddShiftFrom = ({ onCancel }) => {
                     <DialogTitle color="primary.800">Add new shift</DialogTitle>
                     <Divider />
                     <DialogContent sx={{ p: 2 }}>
-                        <Grid container spacing={gridSpacing} justifyContent="center" alignItems="center">
+                        <Grid container spacing={gridSpacing} justifyContent="space-between" alignItems="center">
                             <Grid item xs={12}>
                                 <TextField
                                     fullWidth
@@ -124,52 +132,90 @@ const AddShiftFrom = ({ onCancel }) => {
                                     )}
                                 />
                             </Grid>
-                            {formik.values.roles.map((role, index) => (
-                                <Fragment key={index}>
-                                    <Grid item xs={6} sm={3}>
-                                        <TextField
-                                            name={`roles.${index}.roleType`}
-                                            label="Role Type"
-                                            fullWidth
-                                            error={formik.touched.roles && Boolean(formik.errors.roles)}
-                                            helperText={formik.touched.roles && formik.errors.roles}
-                                        />
-                                    </Grid>
 
-                                    <Grid item xs={6} sm={3}>
-                                        <TextField
-                                            name={`roles.${index}.amount`}
-                                            label="Amount"
-                                            type="number"
-                                            fullWidth
-                                            error={formik.touched.roles && Boolean(formik.errors.roles)}
-                                            helperText={formik.touched.roles && formik.errors.roles}
-                                        />
-                                    </Grid>
-                                </Fragment>
-                            ))}
+                            <FieldArray name="roles">
+                                {({ push, remove }) => (
+                                    <>
+                                        {values.roles.map((roles, index) => (
+                                            <Fragment key={index}>
+                                                <Grid item xs={5.3} sm={5.3}>
+                                                    <Field
+                                                        name={`roles[${index}].roleType`}
+                                                        label="role Type"
+                                                        as={TextField}
+                                                        fullWidth
+                                                        error={
+                                                            touched.roles &&
+                                                            touched.roles[index] &&
+                                                            errors.roles &&
+                                                            errors.roles[index]?.roleType
+                                                        }
+                                                        helperText={
+                                                            touched.roles &&
+                                                            touched.roles[index] &&
+                                                            errors.roles &&
+                                                            errors.roles[index]?.roleType
+                                                        }
+                                                    />
+                                                </Grid>
+                                                <Grid item xs={5.3} sm={5.3}>
+                                                    <Field
+                                                        name={`roles[${index}].amount`}
+                                                        label="Amount"
+                                                        as={TextField}
+                                                        type="number"
+                                                        fullWidth
+                                                        error={
+                                                            touched.roles &&
+                                                            touched.roles[index] &&
+                                                            errors.roles &&
+                                                            errors.roles[index]?.amount
+                                                        }
+                                                        helperText={
+                                                            touched.roles &&
+                                                            touched.roles[index] &&
+                                                            errors.roles &&
+                                                            errors.roles[index]?.amount
+                                                        }
+                                                    />
+                                                </Grid>
+                                                <Grid item xs={1} sm={1} sx={{ mr: 2 }}>
+                                                    <IconButton
+                                                        variant="outlined"
+                                                        sx={{ fontWeight: 'bold' }}
+                                                        onClick={() => remove(index)}
+                                                    >
+                                                        <DeleteForeverOutlinedIcon sx={{ color: theme.palette.primary[800] }} />
+                                                    </IconButton>
+                                                </Grid>
+                                            </Fragment>
+                                        ))}
+                                        <Grid container justifyContent="center" alignItems="center" sx={{ pt: 3, ml: 3.2 }}>
+                                            <Button
+                                                type="button"
+                                                onClick={() => push({ name: '', amount: '' })}
+                                                variant="contained"
+                                                sx={{
+                                                    width: '20%',
+                                                    backgroundColor: theme.palette.grey[300],
+                                                    // eslint-disable-next-line
+                                                    color: theme.palette.common['black'],
+                                                    '&:hover': {
+                                                        background: theme.palette.grey[200]
+                                                    }
+                                                }}
+                                            >
+                                                Add Role
+                                            </Button>
+                                        </Grid>
+                                    </>
+                                )}
+                            </FieldArray>
                         </Grid>
                     </DialogContent>
-                    <DialogActions sx={{ p: 3 }}>
+                    <DialogActions sx={{ p: 2 }}>
                         <Grid container justifyContent="center" alignItems="center">
-                            <Button
-                                type="button"
-                                variant="contained"
-                                sx={{
-                                    width: '30%',
-                                    mr: 1,
-                                    backgroundColor: theme.palette.grey[300],
-                                    // eslint-disable-next-line
-                                    color: theme.palette.common['black'],
-                                    '&:hover': {
-                                        background: theme.palette.grey[200]
-                                    }
-                                }}
-                                onClick={() => formik.setFieldValue('roles', [...formik.values.roles, { roleType: '', amount: '' }])}
-                            >
-                                Add Role
-                            </Button>
-                            <Button type="submit" variant="contained" color="secondary" sx={{ width: '30%', ml: 1 }}>
+                            <Button type="submit" variant="contained" color="secondary" sx={{ width: '30%' }}>
                                 Save
                             </Button>
                         </Grid>
