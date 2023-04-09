71e70ede88cc134eb1a3fa71a2a686b4d2522fae
shahar <shaharl44@gmail.com>
2023-04-09 13:11:11 -0700

    add color generator

--- front/src/components/shifts/index.js
+++ front/src/components/shifts/index.js
@@ -1,30 +1,27 @@
 import { lazy, useState, useEffect } from 'react';
 import Loadable from 'components/Loadable';
-import value from 'assets/scss/_themes-vars.module.scss';
-import { Dialog } from '@mui/material';
+
 import { getMonthlyShifts } from 'utils/api';
 import useAuth from 'hooks/useAuth';
-import AddShiftFrom from './AddShiftFrom';
+import { colorGenerator } from 'utils/color-generator';
 
 const Calendar = Loadable(lazy(() => import('components/calendar')));
 
 const Shifts = () => {
-    const [selectedEvent, setSelectedEvent] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
     const [events, setEvents] = useState([]);
     const { user } = useAuth();
 
     // TODO: get shifts by month (only if permenant shifts generated) + by organization
     useEffect(() => {
         const getShifts = async () => {
-            /* eslint-disable-next-line */
             const result = await getMonthlyShifts(user.organization);
             let parsedData = [];
-            result.data.map((item) =>
+
+            result.data.map(async (item) =>
                 parsedData.push({
                     // eslint-disable-next-line
                     id: item._id,
-                    color: value.secondary200,
+                    color: await colorGenerator(item.startTime.toString()),
                     description: item.name,
                     start: new Date(item.startTime.toString()),
                     end: new Date(item.endTime.toString()),
