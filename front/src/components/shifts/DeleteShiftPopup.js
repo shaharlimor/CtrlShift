eba625ffb8e35a18b9cf7bed79ff3e4bcf182569
shahar <shaharl44@gmail.com>
2023-04-12 13:18:26 -0700

    ui for delete shift

--- front/src/components/shifts/DeleteShiftPopup.js
+++ front/src/components/shifts/DeleteShiftPopup.js
@@ -1,63 +1,16 @@
 import PropTypes from 'prop-types';
 import { Fragment } from 'react';
-import { Button, DialogActions, DialogContent, DialogTitle, Divider, Grid, TextField, MenuItem, Typography } from '@mui/material';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { Button, DialogActions, DialogContent, DialogTitle, Divider, Grid, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-
-import * as Yup from 'yup';
-import { useFormik, Form, FormikProvider } from 'formik';
 
 // project imports
 import { gridSpacing } from 'store/constant';
-import { addConstraint, employeeHasConstraintInShift } from 'utils/api';
+// import { addConstraint, employeeHasConstraintInShift } from 'utils/api';
 import useAuth from 'hooks/useAuth';
-
-// constant
-const getInitialValues = () => {
-    const newEvent = {
-        level: '1',
-        comment: ''
-    };
-    return newEvent;
-};
 
 const DeleteShiftPopup = ({ event, onCancel }) => {
     const { user } = useAuth();
     const theme = useTheme();
-
-    const EventSchema = Yup.object().shape({
-        level: Yup.number(),
-        comment: Yup.string().max(5000)
-    });
-
-    const formik = useFormik({
-        initialValues: getInitialValues(event),
-        validationSchema: EventSchema,
-        onSubmit: async (values, { resetForm, setSubmitting }) => {
-            try {
-                /* eslint-disable-next-line */
-                const alreadyHasConstraint = await employeeHasConstraintInShift(user._id, event.id);
-                if (!alreadyHasConstraint.data) {
-                    /* eslint-disable */
-                    const data = {
-                        level: values.level,
-                        description: values.comment,
-                        shiftId: event.id,
-                        employeeId: user._id
-                    };
-                    /* eslint-enable */
-                    await addConstraint(data);
-
-                    resetForm();
-                    onCancel();
-                    setSubmitting(false);
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    });
 
     return (
         // eslint-disable-next-line
