5cf2226ed093e775e8b06861db163f555b1bfac4
shahar <shaharl44@gmail.com>
2023-05-06 15:35:43 +0300

    fix condole error

--- front/src/components/shifts/selectedShift/TeamMembers.js
+++ front/src/components/shifts/selectedShift/TeamMembers.js
@@ -5,37 +5,23 @@ import { useEffect, useState } from 'react';
 import { Avatar, Button, CardActions, CardContent, Divider, Grid, Typography } from '@mui/material';
 
 // project imports
-import { gridSpacing } from 'store/constant';
 import { getUsersWithConstraintsInShift } from 'utils/api';
-import RenderGrid from './RenderGrid';
-// assets
-/* eslint-disable */
-import Avatar1 from 'assets/images/user-1.png';
-import Avatar2 from 'assets/images/user-1.png';
-import Avatar3 from 'assets/images/user-1.png';
-import Avatar4 from 'assets/images/user-1.png';
-
-// ===========================|| DATA WIDGET - TEAM MEMBERS CARD ||=========================== //
 
 const TeamMembers = ({ event }) => {
     const [employees, setEmployees] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isModalOpen, setIsModalOpen] = useState(false);
 
     useEffect(() => {
         const getEmp = async () => {
             const result = await getUsersWithConstraintsInShift(event.id);
             let parsedData = [];
-            result.data.map(async (item) =>
-                parsedData.push({
-                    // eslint-disable-next-line
-                    id: item._id,
-                    firstName: item.firstName,
-                    lastName: item.lastName
-                })
-            );
+            parsedData = result.data.map((item) => ({
+                // eslint-disable-next-line
+                id: item._id,
+                firstName: item.firstName,
+                lastName: item.lastName
+            }));
+
             setEmployees(parsedData);
-            setIsModalOpen(true);
             parsedData = [];
         };
         getEmp();
@@ -43,31 +29,37 @@ const TeamMembers = ({ event }) => {
 
     return (
         <CardContent>
-            <Grid container spacing={gridSpacing} alignItems="center">
-                <Grid item>
-                    {isModalOpen &&
-                        employees.map((emp) => (
-                            // eslint-disable-next-line
-                            <Grid key={emp._id} item xs={12}>
-                                <Grid key={emp._id + '1'} container spacing={2} alignItems="center">
-                                    <Grid item>
-                                        <Avatar
-                                            alt={emp.firstName}
-                                            src={`https://controlshift-images.s3.eu-central-1.amazonaws.com/${emp.id}.png`}
-                                        />
-                                    </Grid>
-                                    <Grid item xs>
-                                        <Typography align="left" component="div" variant="subtitle1">
-                                            {emp.firstName}
-                                        </Typography>
-                                        <Typography align="left" component="div" variant="subtitle2">
-                                            {emp.lastName}
-                                        </Typography>
-                                    </Grid>
+            {employees.length === 0 ? (
+                <Typography align="center" component="div" variant="h3">
+                    Everyone is available!
+                </Typography>
+            ) : (
+                ''
+            )}
+            <Grid container spacing={1} alignItems="center">
+                {employees &&
+                    employees.map((emp) => (
+                        /*eslint-disable */
+                        <Grid key={emp.id} item xs={4}>
+                            <Grid container spacing={2} alignItems="center">
+                                <Grid key={emp.LastName} item>
+                                    <Avatar
+                                        alt={emp.firstName}
+                                        src={`https://controlshift-images.s3.eu-central-1.amazonaws.com/${emp.id}.png`}
+                                    />
+                                </Grid>
+                                <Grid key={emp.firstName} item xs>
+                                    <Typography align="left" component="div" variant="subtitle1">
+                                        {emp.firstName}
+                                    </Typography>
+                                    <Typography align="left" component="div" variant="subtitle2">
+                                        {emp.lastName}
+                                    </Typography>
                                 </Grid>
                             </Grid>
-                        ))}
-                </Grid>
+                        </Grid>
+                        /* eslint-enable */
+                    ))}
             </Grid>
         </CardContent>
     );
