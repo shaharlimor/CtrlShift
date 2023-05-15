2522f62e59767f3953c2a1d10306d854f2b16b35
asaf halely <asafhalely@gmail.com>
2023-05-15 23:39:51 +0300

    fix algo

--- server/controllers/Algo.js
+++ server/controllers/Algo.js
@@ -1,7 +1,6 @@
 const MonthlyShifts = require("../models/monthlyShifts");
 
 const scheduleShifts = async (monthlyShifts, users, constraints) => {
-  console.log(monthlyShifts[0].roles)
   const createShiftObject = (shiftsArray, newShift) => {
     const shiftLength = newShift.possibleEmployees.length;
     const shiftIndex = shiftsArray.findIndex(shift => shift.length === shiftLength);
@@ -13,48 +12,36 @@ const scheduleShifts = async (monthlyShifts, users, constraints) => {
       };
       
       shiftsArray.push(newShiftGroup);
-      // TODO change the sort
       shiftsArray.sort((a, b) => a.length - b.length);
     } else {
       const existingShiftGroup = shiftsArray[shiftIndex];
       existingShiftGroup.shifts.push(newShift);
-      // TODO change the sort
       existingShiftGroup.shifts.sort((a, b) => a.startTime - b.startTime);
     }
   }
 
   const assignShiftToEmployee = async(shiftID, roleType, employeeID) => {
     if (employeeID == null) {
-      // MonthlyShifts.findByIdAndUpdate(
-      //   shiftID,
-      //   { $addToSet: { 'roles.$[role].employeeIds': employeeID } },
-      //   {
-      //     arrayFilters: [{ 'role.roleType': roleType }],
-      //     new: true,
-      //     upsert: true,
-      //   },
-      //   (err) => {
-      //     if (err) {
-      //       console.error(err);
-      //     }
-      //   }
-      // );
+      console.log(`no user can do shift ${shiftID} in role ${roleType}`)
     } else {
-      // usersWeight.get(employeeID).given += 1;
-      // MonthlyShifts.findByIdAndUpdate(
-      //   shiftID,
-      //   { $addToSet: { 'roles.$[role].employeeIds': employeeID } },
-      //   {
-      //     arrayFilters: [{ 'role.roleType': roleType }],
-      //     new: true,
-      //     upsert: true,
-      //   },
-      //   (err) => {
-      //     if (err) {
-      //       console.error(err);
-      //     }
-      //   }
-      // );
+      usersWeight.get(employeeID).given += 1;
+      console.log(`user ${employeeID} can do shift ${shiftID} in role ${roleType}`)
+      MonthlyShifts.findByIdAndUpdate(
+        shiftID,
+        { $addToSet: { 'roles.$[role].employeeIds': employeeID } },
+        {
+          arrayFilters: [{ 'role.roleType': roleType }],
+          new: true,
+          upsert: true,
+        }
+      )
+        .exec()
+        .then((updatedShift) => {
+          console.log('Employee ID added successfully:', updatedShift);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     }
   }
 
@@ -66,15 +53,18 @@ const scheduleShifts = async (monthlyShifts, users, constraints) => {
 
       return users.reduce((min, user) => {
         const userWeight = usersWeight.get(user._id);
-        const weight = userWeight.given + userWeight.potential/2;
+        let weight;
+        if (userWeight.given > perfectAmount) {
+          weight = userWeight.given + userWeight.potential/2 + 10*(userWeight.given - perfectAmount);
+        } else {
+          weight = userWeight.given + userWeight.potential/2;
+        }
         return weight < min.weight ? { user, weight } : min;
       }, { user: null, weight: Infinity }).user;
     }
   }
 
-  // TODO save available shifts for each user
   const createShiftsAndEmployeesLists = (monthlyShifts, users, constraints) => {
-    // TODO map for type to users (so it will be faster then going therow all the users)
     const shifts = [];
     for (const shift of monthlyShifts) {
       for (const role of shift.roles) {
@@ -116,28 +106,13 @@ const scheduleShifts = async (monthlyShifts, users, constraints) => {
   // create user weight map
   const usersWeight = new Map();
   users.forEach(user => usersWeight.set(user._id, { given: 0, potential: 0 }))
-  
+
   // Step 1: Create a list for each type containing all the shifts and for each shift a list of all the employees that can do that shift
   // Step 2: Sort all the lists by the amount of employees that can do that shift
   const shifts = createShiftsAndEmployeesLists(monthlyShifts, users, constraints);
 
-
-  // shifts.forEach(shiftArray => {
-  //   shiftArray.shifts.forEach(shift => {
-  //     MonthlyShifts.updateMany(
-  //       { _id: shift.shiftsID },
-  //       { $set: { 'roles.$[].employeeIds': [] } }
-  //     )
-  //       .then(() => {
-  //         console.log('EmployeeIds array updated successfully.');
-  //       })
-  //       .catch((err) => {
-  //         console.error(err);
-  //       });
-  //   })
-  // })
-  // console.log(shifts[0].shifts[0])
-  // console.log(shifts[1].shifts[0])
+  const totalLength = shifts.reduce((acc, curr) => acc + curr.shifts.length, 0);
+  const perfectAmount = totalLength / users.length
 
   while (shifts.length != 0) {
     if (shifts[0].length == 0) {
@@ -147,7 +122,7 @@ const scheduleShifts = async (monthlyShifts, users, constraints) => {
       shifts.splice(0, 1);
     } else {
       const chosenEmployee = chooseEmployee(shifts[0].shifts[0]);
-      assignShiftToEmployee(shifts[0].shifts[0], shift.roleType, chosenEmployee._id);
+      assignShiftToEmployee(shifts[0].shifts[0].shiftsID, shifts[0].shifts[0].roleType, chosenEmployee._id);
 
       if (shifts[0].shifts.length > 1) {
         shifts[0].shifts.splice(0, 1);
