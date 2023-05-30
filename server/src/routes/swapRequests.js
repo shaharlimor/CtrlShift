54552f2fc7f0eed3563024db776d9eb7466dd77c
Sagi Ben Mordechai <79403955+sagi21333@users.noreply.github.com>
2023-05-30 17:39:41 +0300

    Switch request front (#72)
    * adding calling swapRequestApi
    
    * fix swaprequest
    
    * remove yours shifts
    
    * create swapRequest
    
    * remove console.log

--- server/src/routes/swapRequests.js
+++ server/src/routes/swapRequests.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const {swapEmployeesInShift, createSwapRequest } = require("../controllers/swapRequests");
-const middleware = require('../common/auth_middleware');
+const {
+  swapEmployeesInShift,
+  createSwapRequest,
+} = require("../controllers/swapRequests");
+const middleware = require("../common/auth_middleware");
 var router = express.Router();
 
-router.post('/create', middleware, createSwapRequest);
-router.post('/switchShifts', middleware, swapEmployeesInShift);
-
+router.post("/create", middleware, createSwapRequest);
+router.post("/switchShifts", middleware, swapEmployeesInShift);
 
 module.exports = router;
