df1606a4ecc9340a9528b87186d7477527c7d3eb
Hadas <hadas716@gmail.com>
2023-06-01 14:56:29 +0300

    remove forger pass and remeber me

--- front/src/components/auth/AuthLogin.js
+++ front/src/components/auth/AuthLogin.js
@@ -140,22 +140,6 @@ const JWTLogin = ({ loginProp, ...others }) => {
                                 </FormHelperText>
                             )}
                         </FormControl>
-                        <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        checked={checked}
-                                        onChange={(event) => setChecked(event.target.checked)}
-                                        name="checked"
-                                        color="primary"
-                                    />
-                                }
-                                label="Remember me"
-                            />
-                            <Typography variant="subtitle1" color="secondary" sx={{ textDecoration: 'none' }}>
-                                Forgot Password?
-                            </Typography>
-                        </Stack>
                         {errors.submit && (
                             <Box sx={{ mt: 3 }}>
                                 <FormHelperText error>{errors.submit}</FormHelperText>
