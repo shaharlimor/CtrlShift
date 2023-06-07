536b152e03df32c964796d731f027b8602b2443b
shahar <shaharl44@gmail.com>
2023-06-07 19:07:59 +0300

    fixes

--- server/src/routes/data.js
+++ server/src/routes/data.js
@@ -4,42 +4,22 @@ const { deleteData, insertData } = require("../common/insert_data");
 
 var router = express.Router();
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const organization = req.params.organization;
-//     const constraints = await getConstraints(organization);
-//     res.send(constraints);
-//   } catch (err) {
-//     res.send("error occurred to get constraints: " + err);
-//   }
-// });
-
-// router.post("/", async (req, res) => {
-//   try {
-//     const newConstraint = new Constraint(req.body);
-//     const result = await newConstraint.save();
-//     res.send("success adding new constraint" + result);
-//   } catch (err) {
-//     res.send("error adding constraint. error: " + err);
-//   }
-// });
-
 router.delete("/", async (req, res) => {
   try {
     await deleteData();
     res.status(200).send(`success deleted data`);
   } catch (err) {
-    res.status(500).send("Error to delete constraints " + err);
+    res.status(500).send("Error to delete data " + err);
   }
 });
 
 router.post("/", async (req, res) => {
-    try {
-      await insertData();
-      res.status(200).send(`success adding constraints`);
-    } catch (err) {
-      res.status(500).send("Error to add constraints " + err);
-    }
-  });
+  try {
+    await insertData();
+    res.status(200).send(`success adding data`);
+  } catch (err) {
+    res.status(500).send("Error to insert data " + err);
+  }
+});
 
 module.exports = router;
