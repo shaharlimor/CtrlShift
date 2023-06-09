ea7f3efa63d6a26059b5d168e9caf67e8243a110
Sagi Ben Mordechai <79403955+sagi21333@users.noreply.github.com>
2023-06-09 18:58:51 +0300

    Swagger config (#78)
    * add swagger
    
    * update swagger
    
    * generate swagger apis
    
    * update swagger
    
    * update swagger
    
    * fix swagger
    
    * update
    
    * swagger working
    
    * continur swagger
    
    ---------
    
    Co-authored-by: ayabryl <ayabryl@gmail.com>

--- server/src/routes/roleTypes.js
+++ server/src/routes/roleTypes.js
@@ -1,11 +1,123 @@
 const express = require("express");
-const { getRoleTypes, createRole, deleteRole, updateRole} = require("../controllers/roleTypes");
-const middleware = require('../common/auth_middleware');
-var router = express.Router();
+const roleTypesController = require("../controllers/roleTypes");
+const middleware = require("../common/auth_middleware");
+const router = express.Router();
 
-router.get('/:orgId', middleware, getRoleTypes);
-router.post('/create', middleware, createRole);
-router.post('/delete', middleware, deleteRole);
-router.post('/update', middleware, updateRole);
+/**
+ * @swagger
+ * tags:
+ *   name: roleTypes
+ *   description: API endpoints for managing roleTypes
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ShiftRoles:
+ *       type: object
+ *       properties:
+ *         organization:
+ *           type: string
+ *         roleType:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * /roleTypes/{orgId}:
+ *   get:
+ *     summary: Get role types by organization ID
+ *     tags: [roleTypes]
+ *     parameters:
+ *       - in: path
+ *         name: orgId
+ *         description: ID of the organization
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       '200':
+ *         description: Role types retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ShiftRoles'
+ *       '500':
+ *         description: Error occurred while retrieving role types
+ */
+router.get("/:orgId", middleware, roleTypesController.getRoleTypes);
+
+/**
+ * @swagger
+ * /roleTypes/create:
+ *   post:
+ *     summary: Create a new role
+ *     tags: [roleTypes]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ShiftRoles'
+ *     responses:
+ *       '200':
+ *         description: Role created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ShiftRoles'
+ *       '500':
+ *         description: Error occurred while creating a role
+ */
+router.post("/create", middleware, roleTypesController.createRole);
+
+/**
+ * @swagger
+ * /roleTypes/delete:
+ *   post:
+ *     summary: Delete a role
+ *     tags: [roleTypes]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: Role deleted successfully
+ *       '404':
+ *         description: Role not found
+ *       '500':
+ *         description: Error occurred while deleting a role
+ */
+router.post("/delete", middleware, roleTypesController.deleteRole);
+
+/**
+ * @swagger
+ * /roleTypes/update:
+ *   post:
+ *     summary: Update a role
+ *     tags: [roleTypes]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ShiftRoles'
+ *     responses:
+ *       '200':
+ *         description: Role updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ShiftRoles'
+ *       '404':
+ *         description: Role not found
+ *       '500':
+ *         description: Error occurred while updating a role
+ */
+router.post("/update", middleware, roleTypesController.updateRole);
 
 module.exports = router;
---
+++ server/src/swagger.js
@@ -0,0 +1,20 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "My API",
+      version: "1.0.0",
+      description: "Description of my API",
+    },
+  },
+  apis: ["./server.js", "./src/routes/*.js"], // Path to your route files
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+};
