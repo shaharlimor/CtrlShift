b611958a662a54bd31dce8d6c1f38d9f797d95ff
Hadas <hadas716@gmail.com>
2023-06-10 18:05:37 +0300

    fix details not updated

--- front/src/utils/userApi.js
+++ front/src/utils/userApi.js
@@ -1,20 +1,14 @@
 import axiosServices from './axios';
 
 export const updateUserDetails = async (email, firstName, lastName, phone) => {
-    try {
-        const response = await axiosServices.post('/auth/updateUserDetails', {
-            email,
-            firstName,
-            lastName,
-            phone
-        });
+    const response = await axiosServices.post('/auth/updateUserDetails', {
+        email,
+        firstName,
+        lastName,
+        phone
+    });
 
-        // Handle the response (e.g., show a success message)
-        console.log(response.data.message);
-    } catch (error) {
-        // Handle the error (e.g., show an error message)
-        console.error('Error updating user details:', error);
-    }
+    return response;
 };
 
 export const deleteUser = async (id) => {
