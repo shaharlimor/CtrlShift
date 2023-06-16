e3fa137002c7f085d59de803224d6f22fe2bf6a9
shahar <shaharl44@gmail.com>
2023-06-16 18:51:52 +0300

    fix click on logo still mark sidebar

--- front/src/layout/MainLayout/Logo/index.js
+++ front/src/layout/MainLayout/Logo/index.js
@@ -3,11 +3,21 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Link } from '@mui/material';
 
 import logo from 'assets/images/logo.jpg';
+import { activeItem } from 'store/slices/menu';
+import { useDispatch } from 'store';
 
-const LogoSection = () => (
-    <Link component={RouterLink} to="/shifts-board">
-        <img src={logo} alt="CtrlShift" width="160" />
-    </Link>
-);
+/* eslint-disable */
+const LogoSection = () => {
+    const dispatch = useDispatch();
+    const itemHandler = (id) => {
+        dispatch(activeItem([id]));
+    };
+
+    return (
+        <Link component={RouterLink} to="/shifts-board">
+            <img src={logo} alt="CtrlShift" width="160" onClick={() => itemHandler('Shifts-Board')} />
+        </Link>
+    );
+};
 
 export default LogoSection;
